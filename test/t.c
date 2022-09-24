f69b2b76766ee070791cc24a56ddd5356dcfd8d7
Dizab67 <dizabanik@gmail.com>
2022-09-24 11:50:40 +0200

    dev0.0.31

--- test/t.c
+++ test/t.c
@@ -3,9 +3,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #define ArrLeng(x)  (sizeof(x) / sizeof((x)[0]))
+
 int main(){
-    long double d;
-    d = 3.0190689000005070900009999;
-    printf("%Lf\n", d);
+    
+    
     return 0;
 }
